a46b4d1d3b0a4f84f82f26e43a6f947012c50fe0
Jaymin <jiangcanming1992@gmail.com>
2026-02-06 01:05:48 +0800

    Fix KKJSBridge crash by ensuring prompt completionHandler is called on dealloc

--- KKJSBridge/KKJSBridge/Dispatcher/KKJSBridgeMessage.m
+++ KKJSBridge/KKJSBridge/Dispatcher/KKJSBridgeMessage.m
@@ -10,5 +10,22 @@
 
 @implementation KKJSBridgeMessage
 
+- (void)dealloc {
+    // 确保 completionHandler 被调用，避免 iOS 抛出异常
+    if (self.callback) {
+        // 必须在主线程调用 WKWebView 相关的 completionHandler
+        void (^callback)(NSDictionary * _Nullable) = self.callback;
+        self.callback = nil;  // 防止重复调用
+
+        if ([NSThread isMainThread]) {
+            callback(nil);
+        } else {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                callback(nil);
+            });
+        }
+    }
+}
+
 @end
 
